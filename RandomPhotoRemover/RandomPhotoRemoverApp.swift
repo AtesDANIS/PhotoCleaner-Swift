87be76670404df8c930a023b26dcc09112c0a060
AtesDANIS <jetates@gmail.com>
2025-04-27 11:10:41 +0300

    Initial Commit

---
+++ RandomPhotoRemover/RandomPhotoRemoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPhotoRemoverApp.swift
+//  RandomPhotoRemover
+//
+//  Created by Ates on 27.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct RandomPhotoRemoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
